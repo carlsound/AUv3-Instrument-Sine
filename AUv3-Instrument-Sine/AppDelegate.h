1623742d21d2a5b68bd72e8e839a2998724cee06
John <john.carlson@carlsound.net>
2019-07-14 23:23:40 -0500

    Initial Commit

---
+++ AUv3-Instrument-Sine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AUv3-Instrument-Sine
+//
+//  Created by John Carlson on 7/14/19.
+//  Copyright © 2019 John Carlson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AUv3-Instrument-Sine/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AUv3-Instrument-Sine
+//
+//  Created by John Carlson on 7/14/19.
+//  Copyright © 2019 John Carlson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
